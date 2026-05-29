e1e4601b343c75e495a34fe94c2034e9801718b2
imrpittala <raghavendrapittala4@gmail.com>
2026-05-29 10:43:39 +0530

    refactor(native): move Google Maps API key to environment variables and dynamic config

---
+++ SafeRoute_Native/app.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  expo: {
+    name: "SafeRoute",
+    slug: "SafeRoute",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || ""
+      }
+    },
+    android: {
+      package: "com.anonymous.SafeRoute",
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || ""
+        }
+      }
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          android: {
+            useLegacyPackaging: true
+          }
+        }
+      ],
+      [
+        "@rnmapbox/maps",
+        {
+          RNMapboxMapsImpl: "mapbox",
+          RNMapboxMapsDownloadToken: "MAPBOX_DOWNLOADS_TOKEN_PLACEHOLDER"
+        }
+      ]
+    ]
+  }
+};
